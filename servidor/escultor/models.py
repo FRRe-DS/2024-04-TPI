1cd6bd5c80d046f2ffd3bd42c44d9cf143ac28ac
b4ngi <ignaciobenetucci@gmail.com>
2024-10-11 19:08:34 -0300

    creacion inicial de aplicaciones

---
+++ servidor/escultor/models.py
@@ -0,0 +1,11 @@
+from django.db import models
+
+class Escultor(models.Model):
+    nombre = models.CharField(max_length=255)
+    nacionalidad = models.CharField(max_length=255)
+    biografia = models.CharField(max_length=255, null=True)
+    contacto = models.CharField(max_length=255, null=True)
+    fecha_nacimiento = models.DateField()
+
+    def __str__(self):
+        return self.nombre
---
+++ servidor/escultura/models.py
@@ -0,0 +1,12 @@
+from django.db import models
+from models import Escultor
+
+class Escultura(models.Model):
+    escultor = models.ForeignKey(Escultor, on_delete=models.CASCADE)
+    titulo = models.CharField(max_length=255)
+    estilo = models.CharField(max_length=255)
+    descripcion = models.TextField(null=True)
+    fecha_creacion = models.DateField()
+
+    def __str__(self):
+        return self.titulo
