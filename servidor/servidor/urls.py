5ba4dc371b27064be0a63aed5a86145de9cdbe2c
b4ngi <ignaciobenetucci@gmail.com>
2024-10-14 03:09:34 -0300

    configuracion de swagger

--- servidor/servidor/urls.py
+++ servidor/servidor/urls.py
@@ -18,9 +18,25 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
+from rest_framework import permissions
+from drf_yasg.views import get_schema_view
+from drf_yasg import openapi
+
+schema_view = get_schema_view(
+    openapi.Info(
+        title="Trabajo Practico Final 2024 - Grupo 4",
+        default_version='v1',
+        description="https://github.com/FRRe-DS/2024-04-TPI",
+    ),
+    public=True,
+    permission_classes=(permissions.AllowAny,),
+)
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
+    path('redoc/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
+
     path('api/esculturas/', include('escultura.urls')),  # Rutas para esculturas
     path('api/escultores/', include('escultor.urls')),  # Rutas para escultores
     path('api/eventos/', include('evento.urls')), #Rutas para eventos
